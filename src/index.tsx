62f538755f47f7268e4e960532c74d99c4951cc3
chaitanyamean <chaitanyamean@gmail.com>
2021-05-01 22:03:29 +0530

    unpac ts file creation

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,71 @@
-import React from 'react';
+import * as esbuild from 'esbuild-wasm'
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {useState, useEffect, useRef} from 'react'
+
+import {unpkgPathPlugin} from './plugins/unpak-path-plugin';
+
+
+const App = () => {
+    const [input , setInput] = useState('');
+    const [code , setCode] = useState('');
+    const ref = useRef<any>()
+
+
+    const onClick = async () => {
+        console.log(input)
+        if(!ref.current) {
+            return;
+        }
+    //    const result =  await ref.current.transform(input, {
+    //         loader: 'jsx',
+    //         target: 'es2015'
+    //     });
+
+    const result = await ref.current.build({
+        entryPoints: ['index.js'],
+        bundle: true,
+        write: false,
+        plugins: [unpkgPathPlugin()]
+    })
+        console.log(result)
+        setCode(result.outputFiles[0].text);
+
+    };
+
+    
+   
+    const startService = async () => {
+        ref.current = await esbuild.startService({
+            worker: true,
+            wasmURL: '/esbuild.wasm'
+        });
+
+        // console.log(service);
+
+    };
+
+    useEffect (() => {
+        startService()
+    }, [])
+
+    return(
+        <div>
+            <textarea 
+            value = {input}
+            onChange={e => setInput(e.target.value)}
+            ></textarea>
+            <div>
+                <button
+                onClick ={onClick}
+                >Submit</button>
+            </div>
+            <pre>{code}</pre>
+        </div>
+    );
+};
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />,
+    document.querySelector('#root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
